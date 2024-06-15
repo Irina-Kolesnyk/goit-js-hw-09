1b253b201defe5496eb185ccec9cee96bb1f4ab1
Irina Kolesnyk <157540775+Irina-Kolesnyk@users.noreply.github.com>
2024-06-15 13:49:56 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,35 +2,57 @@ const formData = {
 email: "",
 message: ""
 };
-const form = document.querySelector('.feedback-form');
-const savedData = localStorage.getItem('feedback-form-state');
-if (savedData) {
-const parsedData = JSON.parse(savedData);
-formData.email = parsedData.email || "";
-formData.message = parsedData.message || "";
-form.elements.email.value = formData.email;
-form.elements.message.value = formData.message;
-}
-form.addEventListener('input', handleInput);
-form.addEventListener('submit', handleSubmit);
 
-function handleInput(event) {
-formData[event.target.name] = event.target.value;
-localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}
+const form = document.querySelector(".feedback-form");
 
-function handleSubmit(event) {
-event.preventDefault();
+form.addEventListener('input', () => {
+const email = form.elements.email.value;
+const message = form.elements.message.value;
+
+formData.email = email.trim();
+formData.message = message.trim();
+
+saveToLS('feedback-form-state', formData);
+});
+
+function saveToLS(key, value) {
+const jsonData = JSON.stringify(value);
+localStorage.setItem(key, jsonData);
+};
+
+function loadFromLS(key) {
+const json = localStorage.getItem(key);
+try {
+const data = JSON.parse(json);
+return data;
+} catch {
+return json;
+}
+};
+
+window.addEventListener('DOMContentLoaded', () => {
+const formData = loadFromLS('feedback-form-state');
+  
+form.elements.email.value = formData?.email || "";
+form.elements.message.value = formData?.message || "";
+});
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+
+const email = form.elements.email.value;
+const message = form.elements.message.value;
+
+formData.email = email.trim();
+formData.message = message.trim();
 
 if (!formData.email || !formData.message) {
 alert('Fill please all fields');
 return;
 }
-saveToLS('feedback-form-state', formData);
-console.log('Form data:', formData);
+  
+console.log(formData);  
 localStorage.removeItem('feedback-form-state');
-formData.email = "";
-formData.message = "";
-form.reset();
-}
+form.reset();  
+});
 
